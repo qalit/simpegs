b0dee00c0262339e7f6d96255ab45aa1b06a5877
qalit <qal.smith@gmail.com>
2013-11-23 22:32:20 +0700

    add templating

---
+++ application/views/templates/footer.php
@@ -0,0 +1,7 @@
+	<!-- footer -->
+	<footer class="footer">
+	<p>Copyleft &copy; 2013. Developed by Al Qalit. href="http://koding.com/qalit"</p> 
+	</footer>
+
+	</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+
+<html lang="en">
+<head>
+	<title>Dashboard SIMPEG</title>
+	
+	<!-- style bootsrap -->
+	<link href="<?php echo site_url();?>assets/css/bootstrap.css" rel="stylesheet"/>
+	<link href="<?php echo site_url();?>assets/css/bootstrap-responsive.css" rel="stylesheet"/>
+	
+	<!-- javascript -->
+	<script type="text/javascript" src="<?php echo site_url();?> assets/js/jquery.min.js"></script>
+	
+</head>
+<body>	
