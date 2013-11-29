5883c9b3017bc4b0887ba47b982d1d154796aea7
qalit <qal.smith@gmail.com>
2013-11-30 00:31:52 +0700

    add header, footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,43 +1,42 @@
-	<!-- footer -->
-	<div id="footer">
-		<p>Alright reserved &copy; 2013. Developed by Al Qalit</p> 
-		<div class="span pull-right">
-			<span class="go-top"><i class="icon-arrow-up"></i></span>
-		</div>
+	<!-- BEGIN FOOTER -->
+	<div id="footer" align="center">
+	Alright reserved &copy; 2013. Developed by Al Qalit.
+	<div class="span pull-right">
+  	</div>
 	</div>
 	<!-- END FOOTER -->
 	<!-- BEGIN JAVASCRIPTS -->
 	<!-- Load javascripts at bottom, this will reduce page load time -->
-	<script src="js/jquery-1.8.3.min.js"></script>
-	<script src="assets/jquery-slimscroll/jquery-ui-1.9.2.custom.min.js"></script>
-	<script src="assets/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-	<script src="assets/fullcalendar/fullcalendar/fullcalendar.min.js"></script>
-	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
-	<script src="js/jquery.blockui.js"></script>
-	<script src="js/jquery.cookie.js"></script>
+	<script src="<?php echo site_url(); ?>assets/js/jquery-1.8.3.min.js"></script>
+	<script src="<?php echo site_url(); ?>assets/jquery-slimscroll/jquery-ui-1.9.2.custom.min.js"></script>
+	<script src="<?php echo site_url(); ?>assets/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="<?php echo site_url(); ?>assets/fullcalendar/fullcalendar/fullcalendar.min.js"></script>
+	<script src="<?php echo site_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="<?php echo site_url(); ?>assets/js/jquery.blockui.js"></script>
+	<script src="<?php echo site_url(); ?>assets/js/jquery.cookie.js"></script>
 	<!-- ie8 fixes -->
 	<!--[if lt IE 9]>
-	<script src="js/excanvas.js"></script>
-	<script src="js/respond.js"></script>
+	<script src="<?php echo site_url(); ?>assets/js/excanvas.js"></script>
+	<script src="<?php echo site_url(); ?>assets/js/respond.js"></script>
 	<![endif]-->
-	<script src="assets/jqvmap/jqvmap/jquery.vmap.js" type="text/javascript"></script>
-	<script src="assets/jqvmap/jqvmap/maps/jquery.vmap.russia.js" type="text/javascript"></script>
-	<script src="assets/jqvmap/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>
-	<script src="assets/jqvmap/jqvmap/maps/jquery.vmap.europe.js" type="text/javascript"></script>
-	<script src="assets/jqvmap/jqvmap/maps/jquery.vmap.germany.js" type="text/javascript"></script>
-	<script src="assets/jqvmap/jqvmap/maps/jquery.vmap.usa.js" type="text/javascript"></script>
-	<script src="assets/jqvmap/jqvmap/data/jquery.vmap.sampledata.js" type="text/javascript"></script>
-	<script src="assets/jquery-knob/js/jquery.knob.js"></script>
-	<script src="assets/flot/jquery.flot.js"></script>
-	<script src="assets/flot/jquery.flot.resize.js"></script>
+	<script src="<?php echo site_url(); ?>assets/jqvmap/jqvmap/jquery.vmap.js" type="text/javascript"></script>
+	<script src="<?php echo site_url(); ?>assets/jqvmap/jqvmap/maps/jquery.vmap.russia.js" type="text/javascript"></script>
+	<script src="<?php echo site_url(); ?>assets/jqvmap/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>
+	<script src="<?php echo site_url(); ?>assets/jqvmap/jqvmap/maps/jquery.vmap.europe.js" type="text/javascript"></script>
+	<script src="<?php echo site_url(); ?>assets/jqvmap/jqvmap/maps/jquery.vmap.germany.js" type="text/javascript"></script>
+	<script src="<?php echo site_url(); ?>assets/jqvmap/jqvmap/maps/jquery.vmap.usa.js" type="text/javascript"></script>
+	<script src="<?php echo site_url(); ?>assets/jqvmap/jqvmap/data/jquery.vmap.sampledata.js" type="text/javascript"></script>
+	<script src="<?php echo site_url(); ?>assets/jquery-knob/js/jquery.knob.js"></script>
+	<script src="<?php echo site_url(); ?>assets/flot/jquery.flot.js"></script>
+	<script src="<?php echo site_url(); ?>assets/flot/jquery.flot.resize.js"></script>
 
-    <script src="assets/flot/jquery.flot.pie.js"></script>
-    <script src="assets/flot/jquery.flot.stack.js"></script>
-    <script src="assets/flot/jquery.flot.crosshair.js"></script>
+    <script src="<?php echo site_url(); ?>assets/flot/jquery.flot.pie.js"></script>
+    <script src="<?php echo site_url(); ?>assets/flot/jquery.flot.stack.js"></script>
+    <script src="<?php echo site_url(); ?>assets/flot/jquery.flot.crosshair.js"></script>
 
-	<script src="js/jquery.peity.min.js"></script>
-	<script type="text/javascript" src="assets/uniform/jquery.uniform.min.js"></script>
-	<script src="js/scripts.js"></script>
+	<script src="<?php echo site_url(); ?>assets/js/jquery.peity.min.js"></script>
+	<script type="text/javascript" src="<?php echo site_url(); ?>assets/uniform/jquery.uniform.min.js"></script>
+	<script src="<?php echo site_url(); ?>assets/js/scripts.js"></script>
 	<script>
 		jQuery(document).ready(function() {
 			// initiate layout and plugins
@@ -48,4 +47,4 @@
 	<!-- END JAVASCRIPTS -->
 </body>
 <!-- END BODY -->
-</html>
+</html>	
